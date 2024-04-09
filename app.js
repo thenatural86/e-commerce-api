1d181d0f2b79ad1387be48f0208842ffd8776e45
thenatural86 <philip.smalls86@gmail.com>
2024-04-09 09:32:41 -0300

    setup basic server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port : ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
