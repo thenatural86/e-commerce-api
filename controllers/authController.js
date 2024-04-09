7da77f22537aab8ba4b0f91848291007c5c3a825
thenatural86 <philip.smalls86@gmail.com>
2024-04-09 17:15:46 -0300

    setup for register user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,11 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+
 const register = async (req, res) => {
-  res.send('Register Route')
+  console.log(req.body)
+  const user = await User.create(req.body)
+  res.json(user)
 }
 
 const login = async (req, res) => {
