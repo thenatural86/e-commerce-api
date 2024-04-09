bcbd69b06e5a281945f8a86542709e469d97d210
thenatural86 <philip.smalls86@gmail.com>
2024-04-09 10:13:55 -0300

    basic route setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 
 // express
 const express = require('express')
@@ -6,6 +7,20 @@ const app = express()
 
 // database
 const connectDB = require('./db/connect')
+
+// middleware
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFound = require('./middleware/not-found')
+
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
