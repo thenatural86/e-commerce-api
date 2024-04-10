5ada3c93c80f884c41815181ac93563f71397893
thenatural86 <philip.smalls86@gmail.com>
2024-04-10 11:06:40 -0300

    jwt sign function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const { email, name, password } = req.body
@@ -15,7 +16,16 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user'
 
   const user = await User.create({ email, name, password, role })
-  res.status(StatusCodes.CREATED).json({ user })
+  const tokenUser = {
+    name: user.name,
+    userId: user._id,
+    role: user.role,
+  }
+  const token = jwt.sign(tokenUser, 'jwtSecret', {
+    expiresIn: '1d',
+  })
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token })
 }
 
 const login = async (req, res) => {
