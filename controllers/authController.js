5408bed824f8607c9948fd7cc69d70d83711e5b6
thenatural86 <philip.smalls86@gmail.com>
2024-04-10 13:09:52 -0300

    move jwt to utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
-const jwt = require('jsonwebtoken')
+const { createJWT } = require('../utils')
 
 const register = async (req, res) => {
   const { email, name, password } = req.body
@@ -21,9 +21,8 @@ const register = async (req, res) => {
     userId: user._id,
     role: user.role,
   }
-  const token = jwt.sign(tokenUser, 'jwtSecret', {
-    expiresIn: '1d',
-  })
+
+  const token = createJWT({ payload: tokenUser })
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser, token })
 }
