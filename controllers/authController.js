96a831512586a0348529ff6e952bbace7bd3fafa
thenatural86 <philip.smalls86@gmail.com>
2024-04-10 10:19:52 -0300

    admin role setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,19 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
 const register = async (req, res) => {
-  console.log(req.body)
-  const { email } = req.body
+  // console.log(req.body)
+  const { email, name, password } = req.body
   const emailAlreadyExist = await User.findOne({ email })
 
   if (emailAlreadyExist) {
     throw new CustomError.BadRequestError('Email already exist!')
   }
-  const user = await User.create(req.body)
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0
+  const role = isFirstAccount ? 'admin' : 'user'
+
+  const user = await User.create({ email, name, password, role })
   res.status(StatusCodes.CREATED).json({ user })
 }
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
