8e1e6b387e524f45c6f621dc70333e73be2c7d1b
thenatural86 <philip.smalls86@gmail.com>
2024-04-12 09:07:04 -0300

    routing for user controller

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const connectDB = require('./db/connect')
 
 // routers
 const authRouter = require('./routes/authRoutes')
+const userRouter = require('./routes/userRoutes')
 
 // middleware
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -33,6 +34,7 @@ app.get('/api/v1', (req, res) => {
 })
 
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
