40c56221cfebcbc374be26d635bf5c09a7919408
thenatural86 <philip.smalls86@gmail.com>
2024-04-12 15:10:22 -0300

    authorize permissions complete

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -16,12 +16,15 @@ const authenticateUser = async (req, res, next) => {
   }
 }
 
-const authorizePermissions = (req, res, next) => {
-  console.log('admin route')
-  if (req.user.role !== 'admin') {
-    throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      )
+    }
+    next()
   }
-  next()
 }
 
 module.exports = { authenticateUser, authorizePermissions }
