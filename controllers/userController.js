3c6653fd8e4d365d312c7962552e83c81e6bc790
thenatural86 <philip.smalls86@gmail.com>
2024-04-13 18:41:06 -0300

    more debugging

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,18 +13,35 @@ const getAllUsers = async (req, res) => {
   res.status(StatusCodes.OK).json({ users })
 }
 const getSingleUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select('-password')
+
   if (!user) {
     throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`)
   }
 
-  const user = await User.findOne({ _id: req.params.id }).select('-password')
   res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user })
 }
 const updateUser = async (req, res) => {
-  res.send('update user')
+  const { email, name } = req.body
+  if (!email || !name) {
+    throw new CustomError.BadRequestError('Please provide all values')
+  }
+
+  const user = await User.findOne({
+    _id: req.user.userId,
+  })
+
+  user.email = email
+  user.name = name
+
+  await user.save()
+
+  const tokenUser = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 const updateUserPassword = async (req, res) => {
   const { oldPassword, newPassword } = req.body
