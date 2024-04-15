7cd34043665674f3394e5647ba863b6c0086a1d8
thenatural86 <philip.smalls86@gmail.com>
2024-04-15 11:22:15 -0300

    create product route

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,26 +2,29 @@ const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
-const createProduct = (req, res) => {
-  res.send('create product route')
+const createProduct = async (req, res) => {
+  req.body.user = req.user.userId
+
+  const product = await Product.create(req.body)
+  res.send({ product })
 }
-const getAllProducts = (req, res) => {
+const getAllProducts = async (req, res) => {
   res.send('get all products route')
 }
 
-const getSingleProduct = (req, res) => {
+const getSingleProduct = async (req, res) => {
   res.send('get single product route')
 }
 
-const updateProduct = (req, res) => {
+const updateProduct = async (req, res) => {
   res.send('update product route')
 }
 
-const deleteProduct = (req, res) => {
+const deleteProduct = async (req, res) => {
   res.send('delete product route')
 }
 
-const uploadImage = (req, res) => {
+const uploadImage = async (req, res) => {
   res.send('upload image route')
 }
 
