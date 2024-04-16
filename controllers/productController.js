b14aae4758b8a631b1a343897a682e02143e8b94
thenatural86 <philip.smalls86@gmail.com>
2024-04-16 11:15:46 -0300

    upload image function

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,7 +55,32 @@ const deleteProduct = async (req, res) => {
 }
 
 const uploadImage = async (req, res) => {
-  res.send('upload image route')
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file uploaded')
+  }
+
+  const productImage = req.files.image
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+
+  const maxSize = 1024 * 1024
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1MB'
+    )
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  )
+
+  await productImage.mv(imagePath)
+
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` })
 }
 
 module.exports = {
