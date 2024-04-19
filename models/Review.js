83a8c25666e2e040f93545ca6bad287e7eb1077f
thenatural86 <philip.smalls86@gmail.com>
2024-04-19 16:41:20 -0300

    review schema attributes

--- models/Review.js
+++ models/Review.js
@@ -6,21 +6,34 @@ const ReviewSchema = new mongoose.Schema(
   {
     rating: {
       type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
     },
     title: {
       type: String,
+      trim: true,
+      required: [true, 'Please provide review title'],
+      maxlength: 100,
     },
     comment: {
       type: String,
+      required: [true, 'Please provide review text'],
     },
     user: {
-      type: User,
+      type: mongoose.Schema.ObjectId,
+      ref: 'user',
+      required: true,
     },
     product: {
-      type: Product,
+      type: mongoose.Schema.ObjectId,
+      ref: 'product',
+      required: true,
     },
   },
   { timestamps: true }
 )
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
 module.exports = mongoose.model('Review', ReviewSchema)
