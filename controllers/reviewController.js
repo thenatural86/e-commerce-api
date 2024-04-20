7b69afc2c19e79289871fb830b783ff8448ff4e0
thenatural86 <philip.smalls86@gmail.com>
2024-04-20 18:13:50 -0300

    update review functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -43,14 +43,32 @@ const getSingleReview = async (req, res) => {
   const review = await Review.findOne({ _id: reviewId })
 
   if (!review) {
-    throw new CustomError.NotFoundError(`No review with id ${reviewId}`)
+    throw new CustomError.NotFoundError(`No review with id : ${reviewId}`)
   }
 
   res.status(StatusCodes.OK).json({ review })
 }
 const updateReview = async (req, res) => {
-  res.send('updateReview')
+  const { id: reviewId } = req.params
+  const { rating, title, comment } = req.body
+
+  const review = await Review.findOne({ _id: reviewId })
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id : ${reviewId}`)
+  }
+
+  checkPermissions(req.user, review.user)
+
+  review.rating = rating
+  review.title = title
+  review.comment = comment
+
+  await review.save()
+
+  res.status(StatusCodes.OK).json({ review })
 }
+
 const deleteReview = async (req, res) => {
   const { id: reviewId } = req.params
   const review = await Review.findOne({ _id: reviewId })
