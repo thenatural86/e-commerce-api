5987e367c1ade043f403573b78c3dc6d56aba2b3
thenatural86 <philip.smalls86@gmail.com>
2024-04-24 15:26:27 -0300

    setup order routes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
 
 // middleware
 const notFound = require('./middleware/not-found')
@@ -43,6 +44,7 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
