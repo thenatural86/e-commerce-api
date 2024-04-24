3f5b96d198dd4dfea8b955c09354c6e7f4ded08d
thenatural86 <philip.smalls86@gmail.com>
2024-04-24 10:12:18 -0300

    defines order schema

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose')
 
-const OrderSchema = new mongoose.Schema({
-  tax: {
-    type: Number,
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+    },
+    shippingFee: {
+      type: Number,
+    },
+    subtotal: {
+      type: Number,
+    },
+    orderItems: [],
+    status: { type: String },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: { type: String },
+    paymentId: { type: String },
   },
-})
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Order', OrderSchema)
