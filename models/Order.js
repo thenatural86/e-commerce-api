a0b123be67f88cbf1c0e766562399cf772593d55
thenatural86 <philip.smalls86@gmail.com>
2024-04-24 11:01:18 -0300

    adds attributes to order schema

--- models/Order.js
+++ models/Order.js
@@ -1,25 +1,53 @@
 const mongoose = require('mongoose')
 
+const SingleOrderItemSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+})
+
 const OrderSchema = new mongoose.Schema(
   {
     tax: {
       type: Number,
+      required: true,
     },
     shippingFee: {
       type: Number,
+      required: true,
     },
     subtotal: {
       type: Number,
+      required: true,
     },
-    orderItems: [],
-    status: { type: String },
+    total: {
+      type: Number,
+      required: true,
+    },
+    orderItems: [SingleOrderItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
+    },
     user: {
       type: mongoose.Schema.ObjectId,
       ref: 'User',
       required: true,
     },
-    clientSecret: { type: String },
-    paymentId: { type: String },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+    },
   },
   { timestamps: true }
 )
