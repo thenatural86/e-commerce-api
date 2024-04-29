a5536a8f4c5418d937190b42526b435c56ea8fef
thenatural86 <philip.smalls86@gmail.com>
2024-04-29 10:31:22 -0300

    small fixes

--- app.js
+++ app.js
@@ -41,21 +41,11 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitize())
 
-app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 
 app.use(express.static('./public'))
 app.use(fileUpload())
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-app.get('/api/v1', (req, res) => {
-  console.log(req.signedCookies)
-  res.send('Hello World')
-})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
